692a12db0974e27b3015e22ca56310b832e4d69e
MOHAMMED ASIF K V <mohammedasifkv@gmail.com>
2025-08-13 14:09:17 +0530

    socket.io integated

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,34 @@
+import { Server } from "socket.io";
+
+class SocketService {
+    private _io:Server;
+
+    constructor() {
+        console.log("Init socket service...");
+        this._io=new Server({
+            cors:{
+                allowedHeaders:["*"],
+                origin:"*"
+            },
+        });
+    }
+
+    public initListeners(){
+        const io=this.io
+        console.log("Init socket listneres...");
+
+        io.on("connect",(socket)=>{
+            console.log("New socket connected",socket.id);
+             
+            socket.on("event:message",async({message}:{message:string})=>{
+                console.log("New message received",message);
+            });
+        });
+    }
+
+    get io(){
+        return this._io;
+    }
+}
+
+export default SocketService;
